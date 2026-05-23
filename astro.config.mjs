6dfe5295ccf9ac33dc3819eec907730fe1aec4c2
Andrew Davis <pulpdrew@gmail.com>
2026-05-23 15:22:41 -0400

    publish to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pulpdrew.github.io',
+	base: '/sveltia-poc',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
